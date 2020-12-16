2e402a2e35bfbfb1ebdc95de008594670d7d7f52
Scrubi19 <scrubi.memo@yandex.ru>
2020-12-16 13:04:16 +0700

    add access to operator page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/manager'
+  get 'pages/operator'
   scope '/api' do
     resources :products, only: %i[create index update destroy]
   end
